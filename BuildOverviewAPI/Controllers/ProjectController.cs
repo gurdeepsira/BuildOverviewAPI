637c4ccd67a40b6f1cff787eb7fbe42d2891ac28
Administrator <gurdipsira_1@yahoo.co.uk>
2019-05-02 13:56:11 -0700

    Add project files.

---
+++ BuildOverviewAPI/Controllers/ProjectController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Octopus.Client;
+using Octopus.Client.Model;
+
+namespace BuildOverviewAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+
+        public string server
+        { get; set; }
+
+        public string apiKey
+        { get; set; }
+
+
+        [HttpGet]
+        [Route("api/{ProjectController}")]
+        [ActionName("GetOctopusDetails")]
+        public void GetOctopusDetails(string projectName)
+        {
+            var endpoint = new OctopusServerEndpoint(server, apiKey);
+
+            using (var client = OctopusAsyncClient.Create(endpoint))
+            {
+
+                Task<ProjectResource> project = null;
+
+                try
+                {
+                    project = client.Result.Repository.Projects.FindByName(projectName);
+
+                  
+                }
+
+                catch (Exception ex)
+                {
+
+                }
+
+                finally
+                {
+                    project.Dispose();
+                }
+               
+
+               
+            }
+
+        }
+    }
+}
